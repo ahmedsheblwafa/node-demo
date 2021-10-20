e9b8c7248a4b084eb0e26ffe422576873415aaef
ahmedsheblwafa <ahmedsheblwafa@gmail.com>
2021-10-20 04:02:12 +0200

    adding helmet and compress

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/users",userRouter)
 app.use("/api/login",loginRouter)
 
 app.get("/",(req,res)=>{
-    res.send("<h1>first page</h1>")
+    res.status(200).send("<h1>first page</h1>")
 })
 
 
