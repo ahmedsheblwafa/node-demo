eb6f33d0bf5f53563180f60cd92f3c879d005d0b
ahmedsheblwafa <ahmedsheblwafa@gmail.com>
2021-10-20 11:05:32 +0200

    solve cors problem

--- routes/login.js
+++ routes/login.js
@@ -5,12 +5,14 @@ const {User} = require("../models/users")
 const jwt = require("jsonwebtoken")
 
 router.post("/",async(req,res)=>{
-    
         const {email,pass} = req.body
         const user = await User.findOne({email})
         if(!user) return res.status(400).send("enter valid user and password")
         if(! await bcrypt.compare(pass,user.password)) return res.status(400).send("enter valid user and password")
         const token = await jwt.sign({email:user.email,name:user.name},"123456")
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
         res.send(token)
     
 })
